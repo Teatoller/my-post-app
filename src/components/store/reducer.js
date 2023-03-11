9f163994bccea6db3a0bf80aa75f914e2b025662
steven ennis <stevenennis@stevens-MacBook-Pro.local>
2023-03-11 23:45:10 +0300

    ft(store): store
    - add reducer
    - connect sore to react app

---
+++ src/components/store/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    articles: [
+      { id: 1, title: "Post 1", body: "Donec auctor risus odio, et sagittis ipsum" },
+      { id: 2, title: "Post 2", body: "Cras rutrum quam eget tincidunt faucibus" },
+    ],
+  }
+  
+  const reducer = (state = initialState, action) => {
+    return state
+  }
+  export default reducer
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reducer from "./components/store/reducer";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const store = createStore(reducer);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
